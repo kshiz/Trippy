8666be60b01eacce2176534d69189e7e2e5b6d6a
svr8 <shikhar.vaish90@gmail.com>
2018-09-22 20:53:55 +0530

    Added Materialize

---
+++ public/script/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB0pgxanTNtVwR7IkMRbW77a0ZMfaHMLUg",
+  authDomain: "trippy-b9e5b.firebaseapp.com",
+  databaseURL: "https://trippy-b9e5b.firebaseio.com",
+  projectId: "trippy-b9e5b",
+  storageBucket: "trippy-b9e5b.appspot.com",
+  messagingSenderId: "649295753388"
+};
+firebase.initializeApp(config);
