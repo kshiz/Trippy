c7d02e58d243b186249c186cc827872fe19ade89
kishore-ganesh <kishore2912000@gmail.com>
2018-09-22 20:48:32 +0530

    Modified server files
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,10 +10,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -36,6 +32,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(4000, ()=>{
+
+  console.log("Server successfuly started");
 });
 
 module.exports = app;
